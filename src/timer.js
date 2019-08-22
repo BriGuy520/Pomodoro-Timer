3c6fbfee253d8784ca3ee13bb1dd1671117d0cb4
Brian Falasz <brianfalasz@gmail.com>
2019-08-22 00:23:46 -0500

    made a the color change to red on the circle when the timer is less than a minute

--- src/timer.js
+++ src/timer.js
@@ -12,17 +12,17 @@ const outlineLength = outline.getTotalLength();
 outline.style.strokeDasharray = outlineLength;
 outline.style.strokeDashoffset = outlineLength;
 
-const pomodoroTotal = 10;
-const breakTotal = 10;
+const pomodoroTotal = 1500;
+const breakTotal = 300;
 
-let pomodoroTimer = 10;
-let breakTimer = 10;
+let pomodoroTimer = 1500;
+let breakTimer = 300;
 
 const clocks = (timer, total, color) => {
   let progress = outlineLength - (timer / total) * outlineLength;
 
   outline.style.strokeDashoffset = progress * -1;
-  outline.style.stroke = color;
+  outline.style.stroke = timer <= 60 ? 'red' : color;
 
   let minutes = Math.floor(timer / 60);
   let seconds = Math.floor(timer % 60) < 10 ? `0${Math.floor(timer % 60)}` : Math.floor(timer % 60);
@@ -38,7 +38,7 @@ const beep = () => {
 start.addEventListener('click', () => {
   setInterval(() => {
 
-    if(!isPaused){ 
+    if(!isPaused){
       if(pomodoroTimer >= 0){
         clocks(pomodoroTimer, pomodoroTotal, "#00cc00");
         pomodoroTimer--;
@@ -47,18 +47,17 @@ start.addEventListener('click', () => {
       if(breakTimer >= 0 && pomodoroTimer < 0){
         clocks(breakTimer, breakTotal, "#ff1ac6");
         breakTimer--;
-        console.log("Break", breakTimer);
       }
 
       if(pomodoroTimer === -1 && breakTimer === breakTotal - 1){
-          beep();
+        beep();
       }
 
       if(breakTimer === -1){
         beep();
       }
   
-      if(breakTimer < 0 && pomodoroTimer <= 0){
+      if(breakTimer < 0 && pomodoroTimer < 0){
         pomodoroTimer = pomodoroTotal;
         breakTimer = breakTotal;
       }
